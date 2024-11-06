aa8bdfbdedb5ee388674bc9bdfe5cf396d710575
bayuraditya <bayuraditya35@gmail.com>
2024-11-06 11:59:51 +0800

    rapikan script

--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -164,15 +164,13 @@
     <script src="{{asset('assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
     <script src="{{asset('assets/static/js/pages/dashboard.js')}}"></script>
  
-    <script src="{{asset('assets/extensions/perfect-scrollbar/perfect-scrollbar.min.js')}}"></script>
-    <script src="{{asset('assets/compiled/js/app.js')}}"></script>
-    
 
     
 <!-- Need: Apexcharts -->
 <script src="{{asset('assets/extensions/apexcharts/apexcharts.min.js')}}"></script>
 <script src="{{asset('assets/static/js/pages/dashboard.js')}}"></script>
 <!-- jQuery (CDN) -->
+
 
 
 </body>
