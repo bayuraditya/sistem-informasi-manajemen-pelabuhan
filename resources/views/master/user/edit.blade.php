162319876688f00822b51a3b75c04120c4102c57
bayuraditya <bayuraditya35@gmail.com>
2026-08-12 08:46:43 +0800

    #maintain typo

--- resources/views/master/user/edit.blade.php
+++ resources/views/master/user/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin-app')
 @section('content')
 <div class="page-heading">
-    <h3>Ship</h3>
+    <h3>User</h3>
 </div>
         <div class="card">
             <div class="card-header">
